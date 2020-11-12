cad5bc3e3e3d6b70ea0f4bc2f3ffe6183395e992
Marcos Brandao <marcosv2138@gmail.com>
2020-11-11 23:34:35 -0300

    Criada DLL para conexao com banco de dados na tela de login

--- CriptoHub/Login.cs
+++ CriptoHub/Login.cs
@@ -9,6 +9,7 @@ using System.Windows.Forms;
 using System.Data.SqlClient;
 using static System.Net.Mime.MediaTypeNames;
 using Application = System.Windows.Forms.Application;
+using db;
 
 namespace CriptoHub
 {
@@ -40,31 +41,22 @@ namespace CriptoHub
 
         private void btnEntrar_Click(object sender, EventArgs e)
         {
-            // Preciso arrumar a conexao com DB
-            SqlConnection Conexao = new SqlConnection(@"Data Source=LAPTOP-O50L6FC1\MSSQLSERVER02;Initial Catalog=CRIPTOHUB;Integrated Security=True");
-            Conexao.Open(); //Abrir a conexao
-            string query = "SELECT * FROM USERS_ADM WHERE NAME = '" + txtNomeAdm.Text + "' AND PASSWORD = " + txtSenhaAdm.Text ;
-            SqlDataAdapter dp = new SqlDataAdapter(query,Conexao);
-            DataTable dt = new DataTable();
-            dp.Fill(dt);
 
-            if(dt.Rows.Count == 1)
+            Banco b = new Banco();
+
+            if(b.Login(txtNomeAdm.Text, txtSenhaAdm.Text))
             {
                 Home j2 = new Home();
                 j2.Show();
                 this.Hide();
-                Conexao.Close();
             }
-           
             else
             {
-                MessageBox.Show("Usuario ou Password incorreto", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show("Usuario ou Senha incorreto", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 txtNomeAdm.Text = ""; // limpa as textbox depois de serem verificadas
                 txtSenhaAdm.Text = "";
                 txtNomeAdm.Select(); //cursor ira sinalizar a primeira txtbox
             }
-
-
             
         }
     }
---
+++ db/Banco.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+
+namespace db
+{
+    public class Banco
+    {
+        public bool Login(string Login, string senha)
+        {
+            bool ret = false;
+
+
+            //SqlConnection con = null; 
+            SqlConnection Conexao = new SqlConnection(@"Data Source=LAPTOP-O50L6FC1\MSSQLSERVER02;Initial Catalog=CRIPTOHUB;Integrated Security=True");
+            Conexao.Open(); //Abrir a conexao
+            string query = "SELECT * FROM USERS_ADM WHERE NAME = '" + Login + "' AND PASSWORD = " + senha;
+            SqlDataAdapter dp = new SqlDataAdapter(query, Conexao);
+            DataTable dt = new DataTable();
+            dp.Fill(dt);
+
+            if (dt.Rows.Count == 1) ret = true;
+            Conexao.Close();
+            return ret;
+        }
+    }
+}
